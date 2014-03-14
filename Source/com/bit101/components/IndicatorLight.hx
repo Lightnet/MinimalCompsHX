562c95c1748a80c8d739191a288927c1d10d01a6
John <unknowgame@hotmail.com>
2014-03-14 12:27:15 -0700

    fixed simple bug for IndicatorLight.

--- Source/com/bit101/components/IndicatorLight.hx
+++ Source/com/bit101/components/IndicatorLight.hx
@@ -1,32 +1,32 @@
 /**
-* IndicatorLight.as
-* Keith Peters
-* version 0.9.10
-* 
-* An indicator light that can be turned on, off, or set to flash.
-* 
-* Copyright (c) 2011 Keith Peters
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+ * IndicatorLight.as
+ * Keith Peters
+ * version 0.97
+ * 
+ * An indicator light that can be turned on, off, or set to flash.
+ * 
+ * Copyright (c) 2009 Keith Peters
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-package com.bit101.components;
+package minimalcomps;
 
 import flash.display.DisplayObjectContainer;
 import flash.display.GradientType;
@@ -35,14 +35,19 @@ import flash.events.TimerEvent;
 import flash.geom.Matrix;
 import flash.utils.Timer;
 
-class IndicatorLight extends Component
-{
-	private var _color:Int;
-	private var _lit:Bool = false;
-	private var _label:Label;
-	private var _labelText:String = "";
-	private var _lite:Shape;
-	private var _timer:Timer;
+class IndicatorLight extends Component {
+	
+	public var color(get, set) : UInt;
+	public var isFlashing(get, null) : Bool;
+	public var isLit(get, set) : Bool;
+	public var label(get, set) : String;
+	
+	var _color:UInt;
+	var _lit:Bool ;
+	var _label:Label;
+	var _labelText:String ;
+	var _lite:Shape;
+	var _timer:Timer;
 	
 	/**
 	 * Constructor
@@ -52,8 +57,9 @@ class IndicatorLight extends Component
 	 * @param color The color of this light.
 	 * @param label String containing the label for this component.
 	 */
-	public function new(parent:DisplayObjectContainer = null, xpos:Float = 0, ypos:Float =  0, color:Int = 0xff0000, label:String = "")
-	{
+	public function new(?parent:Dynamic = null, ?xpos:Float = 0, ?ypos:Float =  0, ?color:UInt = 0xff0000, ?label:String = "") {
+		_lit = false;
+		_labelText = "";
 		_color = color;
 		_labelText = label;
 		super(parent, xpos, ypos);
@@ -62,8 +68,7 @@ class IndicatorLight extends Component
 	/**
 	 * Initializes the component.
 	 */
-	override private function init():Void
-	{
+	override function init() {
 		super.init();
 		_timer = new Timer(500);
 		_timer.addEventListener(TimerEvent.TIMER, onTimer);
@@ -72,8 +77,7 @@ class IndicatorLight extends Component
 	/**
 	 * Creates the children for this component
 	 */
-	override private function addChildren():Void
-	{
+	override function addChildren() {
 		_lite = new Shape();
 		addChild(_lite);
 		
@@ -84,21 +88,17 @@ class IndicatorLight extends Component
 	/**
 	 * Draw the light.
 	 */
-	private function drawLite():Void
-	{
-		var colors:Array<Int>;
+	function drawLite() {
+		var colors:Array<UInt>;
 		if(_lit)
-		{
 			colors = [0xffffff, _color];
-		}
 		else
-		{
 			colors = [0xffffff, 0];
-		}
+		
+		var matrix = new Matrix();
+		matrix.createGradientBox(10, 10, 0, -2.5, -2.5);
 		
 		_lite.graphics.clear();
-		var matrix:Matrix = new Matrix();
-		matrix.createGradientBox(10, 10, 0, -2.5, -2.5);
 		_lite.graphics.beginGradientFill(GradientType.RADIAL, colors, [1, 1], [0, 255], matrix);
 		_lite.graphics.drawCircle(5, 5, 5);
 		_lite.graphics.endFill();
@@ -114,14 +114,10 @@ class IndicatorLight extends Component
 	 * Internal timer handler.
 	 * @param event The TimerEvent passed by the system.
 	 */
-	private function onTimer(event:TimerEvent):Void
-	{
+	function onTimer(event:TimerEvent) {
 		_lit = !_lit;
 		draw();
 	}
-	
-	
-	
 	
 	///////////////////////////////////
 	// public methods
@@ -130,8 +126,7 @@ class IndicatorLight extends Component
 	/**
 	 * Draws the visual ui of the component.
 	 */
-	override public function draw():Void
-	{
+	public override function draw() {
 		super.draw();
 		drawLite();
 		
@@ -145,8 +140,7 @@ class IndicatorLight extends Component
 	/**
 	 * Causes the light to flash on and off at the specified interval (milliseconds). A value less than 1 stops the flashing.
 	 */
-	public function flash(interval:Int = 500):Void
-	{
+	public function flash(?interval:Int = 500) {
 		if(interval < 1)
 		{
 			_timer.stop();
@@ -157,9 +151,6 @@ class IndicatorLight extends Component
 		_timer.start();
 	}
 	
-	
-	
-	
 	///////////////////////////////////
 	// getter/setters
 	///////////////////////////////////
@@ -167,57 +158,46 @@ class IndicatorLight extends Component
 	/**
 	 * Sets or gets whether or not the light is lit.
 	 */
-	public var isLit(get, set):Bool;
-	private function set_isLit(value:Bool):Bool
-	{
+	public function set_isLit(value:Bool):Bool {
 		_timer.stop();
 		_lit = value;
 		drawLite();
 		return value;
 	}
-	private function get_isLit():Bool
-	{
+	public function get_isLit():Bool {
 		return _lit;
 	}
 	
 	/**
 	 * Sets / gets the color of this light (when lit).
 	 */
-	public var color(get, set):Int;
-	
-	private function set_color(value:Int):Int
-	{
+	public function set_color(value:UInt):UInt {
 		_color = value;
 		draw();
 		return value;
 	}
-	private function get_color():Int
-	{
+	public function get_color():UInt {
 		return _color;
 	}
 	
 	/**
 	 * Returns whether or not the light is currently flashing.
 	 */
-	public var isFlashing(get, null):Bool;
-	private function get_isFlashing():Bool
-	{
+	public function get_isFlashing():Bool {
 		return _timer.running;
 	}
 	
 	/**
 	 * Sets / gets the label text shown on this component.
 	 */
-	public var label(get, set):String;
-	
-	private function set_label(str:String):String
-	{
+	public function set_label(str:String):String {
 		_labelText = str;
 		draw();
 		return str;
 	}
-	private function get_label():String
-	{
+	public function get_label():String {
 		return _labelText;
 	}
+	
+
 }
